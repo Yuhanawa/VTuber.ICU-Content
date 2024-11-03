c5f888b7f9e88e60733aa736dcc1457fe5112509
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-11-03 21:43:42 +0800

    feat: modify type of heroImage

--- config.ts
+++ config.ts
@@ -3,42 +3,43 @@ import { defineCollection, z } from "astro:content";
 const v = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional().or(z.string().optional()),
 
-    profile: z.object({
-      Avatar: z.string(),
-      HeaderImage: z.string(),
-      Gallery: z.array(z.object({ URL: z.string(), Remark: z.string() })).optional(),
-      OriginalName: z.string(),
-      Nickname: z.array(z.string()).optional(),
-      Species: z.string().optional(),
-      Birthday: z.coerce.date().optional(),
-      Age: z.number().optional(),
-      AppearanceAge: z.number().optional(),
-      Gender: z.string().optional(),
-      Height: z.string().optional(),
-      Weight: z.string().optional(),
-      BloodType: z.string().optional(),
-      MITB: z.string().optional(),
-      Hometown: z.string().optional(),
-      Company: z.string().optional(),
-      Hobbies: z.string().optional(),
-      Tags: z.array(z.string()).optional(),
-      DebutDate: z.array(z.object({ Platform: z.string(), Date: z.coerce.date() })),
-      Retirement: z.object({ URL: z.string(), Date: z.coerce.date() }).optional(),
-      Medias: z.array(z.object({ Platform: z.string(), URL: z.string() })),
-      ExternalLinks: z.array(z.object({ URL: z.string(), Remark: z.string() })).optional(),
-      Description: z.string(),
-      Highlights: z.array(z.object({ URL: z.string(), Remark: z.string() })).optional(),
-      More: z.array(z.record(z.string())).optional(),
-    }).optional(),
-  }),
+      profile: z
+        .object({
+          Avatar: z.string(),
+          Gallery: z.array(z.object({ URL: z.string(), Remark: z.string() })).optional(),
+          OriginalName: z.string(),
+          Nickname: z.array(z.string()).optional(),
+          Species: z.string().optional(),
+          Birthday: z.coerce.date().optional(),
+          Age: z.number().optional(),
+          AppearanceAge: z.number().optional(),
+          Gender: z.string().optional(),
+          Height: z.string().optional(),
+          Weight: z.string().optional(),
+          BloodType: z.string().optional(),
+          MITB: z.string().optional(),
+          Hometown: z.string().optional(),
+          Company: z.string().optional(),
+          Hobbies: z.string().optional(),
+          Tags: z.array(z.string()).optional(),
+          DebutDate: z.array(z.object({ Platform: z.string(), Date: z.coerce.date() })),
+          Retirement: z.object({ URL: z.string(), Date: z.coerce.date() }).optional(),
+          Medias: z.array(z.object({ Platform: z.string(), URL: z.string() })),
+          ExternalLinks: z.array(z.object({ URL: z.string(), Remark: z.string() })).optional(),
+          Description: z.string(),
+          Highlights: z.array(z.object({ URL: z.string(), Remark: z.string() })).optional(),
+          More: z.array(z.record(z.string())).optional(),
+        })
+        .optional(),
+    }),
 });
 
 export const collections = { v };
